65a24510b811709cd5da2d0ed0c8f96099d451e5
ccccccmd <ccccccmd@163.com>
2016-07-25 14:07:37 +0800

    dd

--- EFTest/Controllers/HController.cs
+++ EFTest/Controllers/HController.cs
@@ -10,18 +10,18 @@ namespace EFTest.Controllers
     public class HController : Controller
     {
         // GET: H
-        public void  Index()
+        public void Index()
         {
             DbContext db = new DbContext();
 
-            Cars c = new Cars() {Name = "宝马"};
+            Cars c = new Cars() { Name = "宝马" };
             Cars c1 = new Cars() { Name = "保时捷" };
-//Users u = new Users() {Age = 18, UserName = "admin",InterestCarId = null};
+            //Users u = new Users() {Age = 18, UserName = "admin",InterestCarId = null};
 
-            Users u1 = new Users() { Age = 18, UserName = "guest",InterestCarId  =0};
+            Users u1 = new Users() { Age = 18, UserName = "guest", InterestCarId = 0 };
             db.Carses.Add(c);
             db.Carses.Add(c1);
-           // db.Userses.Add(u);
+            // db.Userses.Add(u);
             db.Userses.Add(u1);
             db.SaveChanges();
             //{"无法确定“EFTest.Users_InterestCar”关系的主体端。添加的多个实体可能主键相同。"}
@@ -49,9 +49,9 @@ namespace EFTest.Controllers
                 car = c.InterestCar == null ? "" : c.InterestCar.Name
             }).ToList();
 
-            var obj2 = db.Userses.GroupJoin(db.Carses, u => u.InterestCarId, c => c.Id, (u, c) => new {u, c})
+            var obj2 = db.Userses.GroupJoin(db.Carses, u => u.InterestCarId, c => c.Id, (u, c) => new { u, c })
                 .SelectMany(p => p.c.DefaultIfEmpty(),
-                    (c, d) => new {name = c.u.UserName, car = d == null ? "" : d.Name}).ToList();
+                    (c, d) => new { name = c.u.UserName, car = d == null ? "" : d.Name }).ToList();
 
         }
     }
